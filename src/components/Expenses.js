1a678a5752d7e217d3aa8e83a77f5dbd4794c2d2
Hazal Uzar <hazaluzar99@gmail.com>
2022-05-20 12:26:23 +0300

    Card container component is created for composition.

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,14 +1,15 @@
 import ExpenseItem from "./ExpenseItem";
 import './Expenses.css'
+import Card from "./Card";
 
 function Expenses(props){
     return (
-        <div className={'expenses'}>
+        <Card className={'expenses'}>
             <ExpenseItem title={props.expense_list[0].title} amount ={props.expense_list[0].amount} date = {props.expense_list[0].date} />
             <ExpenseItem title={props.expense_list[1].title} amount ={props.expense_list[1].amount} date = {props.expense_list[1].date} />
             <ExpenseItem title={props.expense_list[2].title} amount ={props.expense_list[2].amount} date = {props.expense_list[2].date} />
             <ExpenseItem title={props.expense_list[3].title} amount ={props.expense_list[3].amount} date = {props.expense_list[3].date} />
-        </div>
+        </Card>
     )
 }
 
