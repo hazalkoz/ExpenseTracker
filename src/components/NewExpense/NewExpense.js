63429cd3fd15d64fc6da23b1116b135410d06026
Hazal Uzar <hazaluzar99@gmail.com>
2022-05-21 14:03:42 +0300

    The expense form is closeable.

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,18 +1,29 @@
 import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
+import {useState} from "react";
 
 const NewExpense = (props) => {
+    const [isFormOpen,setFormOpen] = useState(false)
+
     const saveExpenseDataHandler = (enteredExpenseData) =>{
         const expenseData = {
             ...enteredExpenseData,
             id: Math.random().toString()
         }
         props.addExpense(expenseData);
+        setFormOpen(false);
     }
-
+    const openFormHandler = () => {
+        setFormOpen(true)
+    };
+    const closeFormHandler = () => {
+        setFormOpen(false)
+    };
     return (
         <div className='new-expense'>
-            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}/>
+            {!isFormOpen && <button onClick={openFormHandler}>Add New Expense</button>}
+            {isFormOpen && <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} onClose={closeFormHandler}/>}
+
         </div>
     );
 };
