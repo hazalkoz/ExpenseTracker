1804b195edddd404afc2a7080dd78f9021979577
Hazal Uzar <hazaluzar99@gmail.com>
2022-05-21 12:29:11 +0300

    List component created for conditional rendering of the expenses

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,23 +3,22 @@ import './Expenses.css'
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import {useState} from "react";
+import ExpensesList from "./ExpensesList";
 
 function Expenses(props){
     const [year,setYear] = useState('2020')
     const setSelectedYear = (year) => {
         setYear(year)
-        console.log(year)
     }
+    const filteredExpenses = props.expense_list.filter(expense=>{
+        return expense.date.getFullYear().toString()===year
+    });
+
     return (
         <div>
             <Card className={'expenses'}>
                 <ExpensesFilter selectedYear={year} onChangeYear={setSelectedYear}/>
-                {props.expense_list.map(expense=>(
-                    <ExpenseItem
-                        key={expense.id}
-                        title={expense.title}
-                        amount ={expense.amount}
-                        date ={expense.date}/>))}
+                <ExpensesList expenses={filteredExpenses}/>
             </Card>
         </div>
     )
