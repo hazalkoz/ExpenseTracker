92add4efa082b047887b549f18e164402114f88c
Hazal Uzar <hazaluzar99@gmail.com>
2022-05-21 10:51:04 +0300

    Expenses filter is added, not filtering the expense list yet.

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,15 +1,25 @@
 import ExpenseItem from "./ExpenseItem";
 import './Expenses.css'
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
+import {useState} from "react";
 
 function Expenses(props){
+    const [year,setYear] = useState('2020')
+    const setSelectedYear = (year) => {
+        setYear(year)
+        console.log(year)
+    }
     return (
-        <Card className={'expenses'}>
-            <ExpenseItem title={props.expense_list[0].title} amount ={props.expense_list[0].amount} date = {props.expense_list[0].date} />
-            <ExpenseItem title={props.expense_list[1].title} amount ={props.expense_list[1].amount} date = {props.expense_list[1].date} />
-            <ExpenseItem title={props.expense_list[2].title} amount ={props.expense_list[2].amount} date = {props.expense_list[2].date} />
-            <ExpenseItem title={props.expense_list[3].title} amount ={props.expense_list[3].amount} date = {props.expense_list[3].date} />
-        </Card>
+        <div>
+            <Card className={'expenses'}>
+                <ExpensesFilter selectedYear={year} onChangeYear={setSelectedYear}/>
+                <ExpenseItem title={props.expense_list[0].title} amount ={props.expense_list[0].amount} date = {props.expense_list[0].date} />
+                <ExpenseItem title={props.expense_list[1].title} amount ={props.expense_list[1].amount} date = {props.expense_list[1].date} />
+                <ExpenseItem title={props.expense_list[2].title} amount ={props.expense_list[2].amount} date = {props.expense_list[2].date} />
+                <ExpenseItem title={props.expense_list[3].title} amount ={props.expense_list[3].amount} date = {props.expense_list[3].date} />
+            </Card>
+        </div>
     )
 }
 
